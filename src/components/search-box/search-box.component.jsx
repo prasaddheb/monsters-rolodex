57b49598d3cc43da87c8db354a4ea3a93a0adb4d
Prasad <dhebprasadshivaji@gmail.com>
2022-02-17 17:06:41 +0530

    monsters-rolodex project complete

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import '../search-box/search-box.styles.css';
+
+export const SearchBox =({placeholder,handlechange})=>(
+    <input 
+    className='search'
+    type='search' placeholder={placeholder} onChange={handlechange}  />
+);
